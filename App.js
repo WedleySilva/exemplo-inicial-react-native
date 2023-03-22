41da5531fcac5178c83f65a27c95424d131a51e4
WedleySilva <WedleySilva2105@gmail.com>
2023-03-22 14:24:19 -0300

    criação header em class

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Usando Funções
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// import { StyleSheet, Text, View } from 'react-native';
+// import Header from './src/components/Header';
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Header />
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+// Usando Classes
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header />
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
